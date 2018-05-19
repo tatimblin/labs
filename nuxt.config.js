a25f81e2a7dfd98b73a4061cde6067922863bc61
Tristan Timblin <tatimblin@gmail.com>
2018-05-19 00:11:18 -0400

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'labs',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Collection of front-end demos' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  module: {
+    rules: [
+      // ... other rules omitted
+
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
---
+++ pages/demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1>This is demo.vue</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+    div {
+        background-color: red;
+    }
+}
+</style>
