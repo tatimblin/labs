048e1d569b963c39ef1a876a3f97044e41b3f756
Tristan Timblin <tatimblin@gmail.com>
2018-05-25 23:44:41 -0400

    leave transition broken

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-    import Navigation from '~/components/Navigation.vue'    
+    import Navigation from '~/components/Navigation.vue'
     
     export default {
         components: {
@@ -18,20 +18,20 @@
 </script>
 
 <style scoped lang="scss">
+$ease: cubic-bezier(0.86, 0, 0.07, 1);
     
 .test-enter-active {
-    animation: acrossIn 2s 2s ease-out both;
+    animation: acrossIn 1s 1s $ease both;
 }
 .test-leave-active {
-    animation: acrossOut 2s ease-in both;
+    animation: acrossOut 2s $ease both;
 } 
-
 @keyframes acrossIn {
     0% {
-        transform: translate3d(-100vw, 0, 0);
+        transform: scale(0.5) translate3d(-100vw, 0, 0);
     }
     100% {
-        transform: translate3d(0, 0, 0);
+        transform: scale(1) translate3d(0, 0, 0);
     }
 }
 @keyframes acrossOut {
@@ -42,5 +42,10 @@
         transform: translate3d(0vw, 0, 0);
     }
 }
-    
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.75s;
+  }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0
+  }
 </style>
