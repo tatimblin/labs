830d08e0256e74dd5a10897aaad4c4be32ffef61
Tristan Timblin <tatimblin@gmail.com>
2018-08-26 11:20:57 -0400

    Featured images on post list.

--- components/SiteList.vue
+++ components/SiteList.vue
@@ -5,11 +5,18 @@
                 <nuxt-link :to="lab.page">{{ lab.title }}</nuxt-link>
             </li>
         </ul>
-        <ul v-else>
-            <li v-for="post in posts" :key="post.date">
-                <nuxt-link :to="post._path">{{ post.title }}</nuxt-link>
-            </li>
-        </ul>
+        <div v-else class="site-list-post">
+            <div class="site-list-post-preview">
+                <transition name="post-preview" mode="in-out">
+                    <img :key="projIndex" :src="`${ posts[projIndex].thumbnail }`">
+                </transition>
+            </div>
+            <ul>
+                <li v-for="(post, index) in posts" :key="post.date" @mouseover="highlightProject(`${index}`)">
+                    <nuxt-link :to="post._path">{{ post.title }}</nuxt-link>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
@@ -29,12 +36,20 @@
             _path: `/project/${key.replace('.json', '').replace('./', '')}`
             }));
 
-            return { posts };
+            return { 
+                posts,
+                projIndex: 0
+            };
         },
         props: {
             listType: {
                 type: String,
                 default: 'post'
+            }
+        },
+        methods: {
+            highlightProject(i) {
+                this.projIndex = i
             }
         }
     }
@@ -42,5 +57,48 @@
 
 <style lang="scss">
 @import '~assets/sass/_variables.scss';
+
+.site-list {
+    text-align: right;
+
+    ul {
+        
+        li {
+            padding: $spacing/2 0;
+        }
+        a {
+            color: $light-txt;
+            text-decoration: none;
+        }
+    }
+    &-post-preview {
+        img {
+            position: absolute;
+            width: 300px;
+            left: 0;
+        }
+    }
+}
+
+
+// Transition lab title
+.post-preview-enter-active {
+    transform-origin: center left;
+    transition: all .3s ease;
+}
+
+.post-preview-leave-active {
+    transition: all .3s ease;
+}
+
+.post-preview-enter {
+    transform: translateY(60px) scale(0.5);
+    opacity: 0 !important;
+}
+
+.post-preview-leave-to {
+    transform: translateY(-60px);
+    opacity: 0 !important;
+}
 
 </style>
