1ab1371c4219dcc72a8b656ad576dca515932713
Tristan Timblin <tatimblin@gmail.com>
2018-09-08 16:33:59 -0400

    list titles

--- components/SiteProcess.vue
+++ components/SiteProcess.vue
@@ -1,29 +1,87 @@
 <template>
+
     <section class="container site-process">
+
         <div class="process-list">
-            <div class="process-list-item_wrap">
-                <div class="process-list-item">
-                    <div class="process-list-item-num">
-                        <h1>01</h1>
-                        <small>Inspiration</small>
-                    </div>
-                    <div class="process-list-item-detail">
-                        <h3>What is this</h3>
-                    </div>
-                    <div class="process-list-item-expand">
-                        +
-                    </div>
+
+            <div v-for="(step, index) in steps" :key="index" class="process-list-item">
+
+                <div class="process-list-item-num">
+
+                    <h1>0{{ index }}</h1>
+
+                    <small>{{ step.title }}</small>
+
                 </div>
+
+                <div class="process-list-item-detail">
+
+                    <h3>{{ step.desc }}</h3>
+
+                </div>
+
+                <div class="process-list-item-expand">
+                    +
+                </div>
+
             </div>
+
         </div>
+
     </section>
+
 </template>
 
 <script>
     
 
     export default {
-        
+        data() {
+            return { 
+                steps: [
+                    {
+                        title: 'Discovery',
+                        desc: 'An initial meeting, focusing on what the product aims to accomplish for potential users.'
+                    },
+                    {
+                        title: 'Requirements',
+                        desc: 'A concise list of everything needed for the product to perform its function for all involved.'
+                    },
+                    {
+                        title: 'Reference',
+                        desc: 'Compiling successful competitive products and relevant examples in other industries.'
+                    },
+                    {
+                        title: 'Journies',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Manuscript',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Wireframe',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Styleguide',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Asset Creation',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Static Design',
+                        desc: 'a desc.'
+                    },
+                    {
+                        title: 'Prototyping',
+                        desc: 'a desc.'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
@@ -35,18 +93,22 @@
     .process-list {
         margin: 0 auto;
         padding: 0 0 $spacing*4 0;
-        
-        &-item_wrap {
-            padding: $spacing*2 0;
-            border-top: 2px solid $light-bg;
-        }
 
         &-item {
+            position: relative;
             display: flex;
+            margin: $spacing*4 0;
             padding: $spacing;
             border-radius: 2px;
             cursor: pointer;
 
+            &:before {
+                content: '';
+                position: absolute;
+                width: 100%;
+                top: -$spacing*2; left: 0;
+                border-top: 1px solid lighten($dark-bg, 10%);
+            }
             &:hover {
                 background-color: lighten($dark-bg, 10%);
 
